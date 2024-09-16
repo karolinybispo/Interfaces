cf2f39a08e3ab7df14661d3c08f299d66a1e1dee
karoliny bispo <karol-bobio@hotmail.com>
2024-09-16 10:29:39 -0300

    cadastro categoria responsiva e com navbar

---
+++ cadastroCategoria/script.js
@@ -0,0 +1,14 @@
+// script.js
+
+document.addEventListener('DOMContentLoaded', function () {
+    const toggler = document.getElementById('navbar-toggler');
+    const collapse = document.getElementById('navbar-collapse');
+
+    toggler.addEventListener('click', function () {
+        if (collapse.style.display === 'block') {
+            collapse.style.display = 'none';
+        } else {
+            collapse.style.display = 'block';
+        }
+    });
+});
