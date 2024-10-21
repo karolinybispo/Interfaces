a96f7015cd1e64efcde1b3ab5ba1a21255668792
karoliny bispo <karol-bobio@hotmail.com>
2024-10-21 13:51:15 -0300

    fase de trazer produtos

--- cardapio/buscarProdutos.php
+++ cardapio/buscarProdutos.php
@@ -1,10 +1,6 @@
 <?php
 include '../conexaoBanco/db_conexao.php'; // Inclua a conexão com o banco
 
-// Verifique a conexão
-if (!$conn) {
-    die("Erro na conexão com o banco de dados: " . mysqli_connect_error());
-}
 
 // Consulta SQL para buscar todos os produtos e suas respectivas categorias
 $sql = "SELECT p.nome_produto, p.descricao, p.preco_produto, c.nome_categoria 
