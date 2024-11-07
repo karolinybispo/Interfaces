a7b29cd2bd2055b011698704db67ad22e04a2fa9
karoliny bispo <karol-bobio@hotmail.com>
2024-11-07 11:09:19 -0300

    login usuario: verificado

--- loginUsuario/processaLogin.php
+++ loginUsuario/processaLogin.php
@@ -6,21 +6,22 @@ error_reporting(E_ALL);
 
     if($_SERVER["REQUEST_METHOD"] == "POST"){
         include '../conexaoBanco/db_conexao.php'; //arq que faz conexao com BD
-        if($mySqli->connect_error){
-            echo "Erro na conexão: " . $mySqli->connect_error; 
+        if($conn->connect_error){
+            echo "Erro na conexão: " . $conn->connect_error; 
         }
 
         $nome = $_POST['nomeInput'];
         $senha = $_POST['senhaInput'];
 
-        $sql = $mySqli->prepare("SELECT id_cliente FROM cliente WHERE nome_cliente = ? AND senha_cliente = ?");
-        $sql -> bind_param("ss", $nome, $senha);
-        $sql->execute(); //excuta a consulta de incluir os valores de $nome e $senha dentro dos ?
-        $sql->store_result(); //guarda o resultado da consulta feita
 
-        if($sql->num_rows > 0) { //se o numero de linhas retornadas for maior que 0, o nome e senha inseridos correspondem ao que esta registrado no banco.
+        $sql = $conn->prepare("SELECT id_cliente FROM cliente WHERE nome_cliente = ? AND senha_cliente = ?");
+        $sql -> bind_param("ss", $nome, $senha);
+        $sql->execute(); //excuta a consulta de incluir os valores de $nome e $senha dentro dos ?.
+        $sql->store_result(); //guarda o resultado da consulta feita.
+
+        if($sql->num_rows > 0) { //se o numero de linhas retornadas for maior que 0, nome e senha inseridos correspondem ao que esta registrado no banco.
             
-            $sql->bind_result($id_cliente); //prepara a variavel $id_cliente para receber o valor da coluna id_cliente do BD
+            $sql->bind_result($id_cliente); //prepara a variavel $id_cliente para receber o valor da coluna id_cliente do BD.
             $sql->fetch();//executa a extracao do valor da consulta e armazena o resultado na variavel $id_cliente
 
             //armazenando o id no localStorege (isso permite o id ser recuperado depois) e depois redirecionando o user
@@ -29,13 +30,14 @@ error_reporting(E_ALL);
                         window.location.href = '../cardapio/cardapio.html';
                  </script>";
         }
+        
         else{
             echo "nome ou senha nao cadastrados";
         }
 
         //encerra consulta e conexao
         $sql->close();
-        $mySqli->close();
+        $conn->close();
 
 }
 ?>
