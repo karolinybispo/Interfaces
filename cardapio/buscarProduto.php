0c7d96991ab0e76c090b9803b2576de8f212507e
karoliny bispo <karol-bobio@hotmail.com>
2024-12-04 16:42:32 -0300

    mais comentarios

--- cardapio/buscarProduto.php
+++ cardapio/buscarProduto.php
@@ -8,7 +8,7 @@
 
 include '../conexaoBanco/db_conexao.php'; // Inclua a conexão com o banco
 
-header('Content-Type: application/json'); // Define o cabeçalho para JSON
+header('Content-Type: application/json'); // Define que a resposta sera enviada em formato json
 
 
 // Consulta SQL para buscar todos os produtos 
@@ -19,7 +19,7 @@ $result = $sql->get_result();
 
 //criando array e armazenado ent tiver valor
 $produtos = [];
-while ($row = $result->fetch_assoc()){
+while ($row = $result->fetch_assoc()){ //fetch_assoc() obtem cada linha do resultado da consulta, uma por vez
     $row['img_proguto'] = 'http://localhost/interfaces/CadProdutos/uploads/' . basename($row['img_proguto']); // necessario para o navegador conseguir acessar a imagem no servidor
     $produtos[] = $row; // cada produto sera adc 
 }
